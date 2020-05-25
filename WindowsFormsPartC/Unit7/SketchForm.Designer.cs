a2508f076250f05f8a60051cf00a85527a691fd2
Eric Man <eric.man95@gmail.com>
2020-05-25 16:50:02 +0100

    Done basic task project

--- WindowsFormsPartC/Unit7/SketchForm.Designer.cs
+++ WindowsFormsPartC/Unit7/SketchForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.sketchPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // sketchPanel
@@ -40,12 +41,24 @@
             this.sketchPanel.TabIndex = 0;
             this.sketchPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.sketchPanel_Paint);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(356, 583);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(557, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Eric\'s Etch a Sketch Project";
+            // 
             // SketchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
             this.ClientSize = new System.Drawing.Size(1098, 663);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.sketchPanel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -54,11 +67,13 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.SketchForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel sketchPanel;
+        private System.Windows.Forms.Label label1;
     }
 }
